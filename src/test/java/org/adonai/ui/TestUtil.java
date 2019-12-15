7d77665141dfe10b7f9bed7d0e3310d31bba6003
OleyMa <markus.oley@vsa.de>
2019-12-15 12:07:49 +0100

    tests

--- src/test/java/org/adonai/ui/TestUtil.java
+++ src/test/java/org/adonai/ui/TestUtil.java
@@ -5,11 +5,13 @@ import java.io.File;
 public class TestUtil {
 
   public static void initialize () {
-    //System.setProperty("testfx.robot", "glass");
-    //System.setProperty("testfx.headless", "true");
-    //System.setProperty("prism.order", "sw");
-    //System.setProperty("prism.text", "t2k");
-    //System.setProperty("java.awt.headless", "true");
+    System.setProperty("testfx.robot", "glass");
+    System.setProperty("testfx.headless", "true");
+    System.setProperty("monocle.platform","Headless");
+    System.setProperty("prism.order", "sw");
+    System.setProperty("prism.text", "t2k");
+    System.setProperty("java.awt.headless", "true");
+    System.setProperty("headless.geometry", "1600x1200-32");
   }
 
   public static File getDefaultTestDataPath () {
