448d57814cf863f2e9c6339b67c39e8488d3c2a6
oleyma <markus.oley@vsa.de>
2021-02-06 21:31:14 +0100

    bugfixing

--- plugins/pluginCore/src/main/java/org/adonai/fx/scope/ScopeTreeProvider.java
+++ plugins/pluginCore/src/main/java/org/adonai/fx/scope/ScopeTreeProvider.java
@@ -24,39 +24,7 @@ public class ScopeTreeProvider {
     ScopeItem scopeItemSongBook = applicationEnvironment.getScopeItem(currentSongBook);
     TreeItem treeItemSongBook = new TreeItem(scopeItemSongBook, scopeItemSongBook.getIcon());
     for (Song song : scopeItemSongBook.getSongBook().getSongs()) {
-      ScopeItem scopeItem = new ScopeItem(scopeItemSongBook, song);
-
-      Label lblId = new Label(song.getId().toString());
-      final int WIDTH_ID = 50;
-      lblId.setMaxWidth(WIDTH_ID);
-      lblId.setMinWidth(WIDTH_ID);
-
-      Label lblName = new Label(song.getName());
-      final int WIDTH_NAME = 400;
-      lblName.setMaxWidth(WIDTH_NAME);
-      lblName.setMinWidth(WIDTH_NAME);
-
-      final int WIDTH_ORIGINKEY = 50;
-      Button btnOriginalKey = new Button();
-      btnOriginalKey.setText(song.getOriginalKey());
-      btnOriginalKey.setMaxWidth(WIDTH_ORIGINKEY);
-      btnOriginalKey.setMinWidth(WIDTH_ORIGINKEY);
-
-      final int WIDTH_CURRENTKEY = 70;
-      Button btnCurrentKey = new Button();
-      btnCurrentKey.setText(song.getCurrentKey() != null ? "->" + song.getCurrentKey(): "");
-      btnCurrentKey.setMaxWidth(WIDTH_CURRENTKEY);
-      btnCurrentKey.setMinWidth(WIDTH_CURRENTKEY);
-
-      final int WIDTH_ADDITIONALS_MP3 = 50;
-      Button btnAdditionalMp3 = new Button();
-      btnAdditionalMp3.setText(song.getCurrentKey() != null ? "->" + song.getCurrentKey(): "");
-      btnAdditionalMp3.setMaxWidth(WIDTH_ADDITIONALS_MP3);
-      btnAdditionalMp3.setMinWidth(WIDTH_ADDITIONALS_MP3);
-      if (song.getAdditional(AdditionalType.AUDIO) != null)
-        btnAdditionalMp3.setGraphic(Consts.createIcon("fas-file-audio", Consts.ICON_SIZE_TOOLBAR));
-
-      TreeItem treeItemSong = new TreeItem(scopeItem, new HBox(5, scopeItem.getIcon(), lblId, lblName, btnOriginalKey, btnCurrentKey, btnAdditionalMp3));
+      TreeItem treeItemSong = createSongTreeItem(scopeItemSongBook, song);
       treeItemSongBook.getChildren().add(treeItemSong);
     }
     rootItem.getChildren().add(treeItemSongBook);
@@ -64,16 +32,53 @@ public class ScopeTreeProvider {
     for (Session nextSession: applicationEnvironment.getCurrentConfiguration().getSessions()) {
       ScopeItem scopeItemSession = applicationEnvironment.getScopeItem(nextSession);
       TreeItem treeItemSession = new TreeItem(scopeItemSession, scopeItemSession.getIcon());
-      for (Integer song : nextSession.getSongs()) {
-        Song nextSong = currentSongBook.findSong(song);
-        ScopeItem scopeItem = new ScopeItem(scopeItemSession, nextSong);
-        TreeItem treeItemSong = new TreeItem(scopeItem, scopeItem.getIcon());
+      for (Integer songId : nextSession.getSongs()) {
+        Song song = currentSongBook.findSong(songId);
+        TreeItem treeItemSong = createSongTreeItem(scopeItemSession, song);
+
         treeItemSession.getChildren().add(treeItemSong);
       }
       rootItem.getChildren().add(treeItemSession);
     }
 
     return rootItem;
+
+  }
+
+  private TreeItem createSongTreeItem (final ScopeItem parent, final Song song) {
+    ScopeItem scopeItem = new ScopeItem(parent, song);
+
+    Label lblId = new Label(song.getId().toString());
+    final int WIDTH_ID = 50;
+    lblId.setMaxWidth(WIDTH_ID);
+    lblId.setMinWidth(WIDTH_ID);
+
+    Label lblName = new Label(song.getName());
+    final int WIDTH_NAME = 400;
+    lblName.setMaxWidth(WIDTH_NAME);
+    lblName.setMinWidth(WIDTH_NAME);
+
+    final int WIDTH_ORIGINKEY = 50;
+    Button btnOriginalKey = new Button();
+    btnOriginalKey.setText(song.getOriginalKey());
+    btnOriginalKey.setMaxWidth(WIDTH_ORIGINKEY);
+    btnOriginalKey.setMinWidth(WIDTH_ORIGINKEY);
+
+    final int WIDTH_CURRENTKEY =75;
+    Button btnCurrentKey = new Button();
+    btnCurrentKey.setText(song.getCurrentKey() != null ? "->" + song.getCurrentKey(): "");
+    btnCurrentKey.setMaxWidth(WIDTH_CURRENTKEY);
+    btnCurrentKey.setMinWidth(WIDTH_CURRENTKEY);
+
+    final int WIDTH_ADDITIONALS_MP3 = 50;
+    Button btnAdditionalMp3 = new Button();
+    btnAdditionalMp3.setText(song.getCurrentKey() != null ? "->" + song.getCurrentKey(): "");
+    btnAdditionalMp3.setMaxWidth(WIDTH_ADDITIONALS_MP3);
+    btnAdditionalMp3.setMinWidth(WIDTH_ADDITIONALS_MP3);
+    if (song.getAdditional(AdditionalType.AUDIO) != null)
+      btnAdditionalMp3.setGraphic(Consts.createIcon("fas-file-audio", Consts.ICON_SIZE_TOOLBAR));
+
+    return new TreeItem(scopeItem, new HBox(5, scopeItem.getIcon(), lblId, lblName, btnOriginalKey, btnCurrentKey, btnAdditionalMp3));
 
   }
 }
