da7531159bacc43872ace240ce05a3191b02f8fc
OleyMa <markus.oley@noventi.de>
2024-01-30 22:51:11 +0100

    tests

--- src/main/java/org/adonai/plugin/publish/fx/initstep/ChooseTenantController.java
+++ src/main/java/org/adonai/plugin/publish/fx/initstep/ChooseTenantController.java
@@ -1,10 +1,14 @@
 package org.adonai.plugin.publish.fx.initstep;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.adonai.ApplicationEnvironment;
 import org.adonai.fx.AbstractController;
@@ -28,6 +32,8 @@ public class ChooseTenantController extends AbstractController {
   @FXML
   Button btnRemoveTenant;
 
+  @FXML Button btnExit;
+
   @FXML
   public void initialize () {
     btnAddTenant.setOnAction(event -> {
@@ -38,6 +44,8 @@ public class ChooseTenantController extends AbstractController {
       lviTenants.getSelectionModel().select(txtNew.getText());
       txtNew.clear();
     });
+
+    btnExit.setOnAction(event -> {System.exit(0);});
 
     btnRemoveTenant.setOnAction(event -> {
       String selectedTenant = lviTenants.getSelectionModel().getSelectedItem();
@@ -50,18 +58,29 @@ public class ChooseTenantController extends AbstractController {
     });
 
     btnChooseTenant.setOnAction(event -> {
-      String selectedTenant = lviTenants.getSelectionModel().getSelectedItem();
-      log.info("Choose tenant button pressed with selected tenant " + selectedTenant);
-      getApplicationEnvironment().getModel().setCurrentTenant(selectedTenant);
-
-      getStage().close();
+      chooseTenant();
     });
+
+    lviTenants.setOnKeyPressed(event -> {
+      if (event.getCode().equals(KeyCode.ENTER)) {
+        chooseTenant();
+      }
+    });
+  }
+
+  public void chooseTenant () {
+    String selectedTenant = lviTenants.getSelectionModel().getSelectedItem();
+    log.info("Choose tenant button pressed with selected tenant " + selectedTenant);
+    getApplicationEnvironment().getModel().setCurrentTenant(selectedTenant);
+
+    getStage().close();
   }
 
   public void setApplicationEnvironment (ApplicationEnvironment applicationEnvironment) {
     super.setApplicationEnvironment(applicationEnvironment);
-    this.lviTenants.setItems(FXCollections.observableArrayList(getApplicationEnvironment().getAllTenants()));
-    this.lviTenants.getSelectionModel().select(getApplicationEnvironment().getAllTenants().iterator().next());
+    lviTenants.setItems(FXCollections.observableArrayList(getApplicationEnvironment().getAllTenants()));
+    lviTenants.getSelectionModel().selectFirst();
+    
   }
 
 
